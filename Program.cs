50ad2e5364c02dc1cc15ee99c12e58a2b4f815b4
pl_konstkri <fcbsport2015@bk.ru>
2024-08-29 16:04:43 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using g3;
+
+namespace geometry_3_Sharp_meshmesh.cut_my_example
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Создаем базовый меш (коробка)
+            DMesh3 boxMesh = new DMesh3();
+            var x = new Vector3d(1, 1, 1);
+            var y = new Vector3d(1, 1, 1);
+            var z = new Vector3d(1, 1, 1);
+            TrivialBox3Generator boxGen = new TrivialBox3Generator()
+            {
+                //Box = new Box3d(Vector3d.Zero, x, y, z, Vector3d.Zero),
+            };
+            boxMesh = boxGen.Generate().MakeDMesh();
+
+            // Создаем второй меш (сфера)
+            DMesh3 sphereMesh = new DMesh3();
+            Sphere3Generator_NormalizedCube sphereGen = new Sphere3Generator_NormalizedCube()
+            {
+                Radius = 0.5f,
+                //Slices = 16,
+                
+            };
+            sphereMesh = sphereGen.Generate().MakeDMesh();
+
+            // Перемещаем сферу, чтобы она пересекалась с коробкой
+            MeshTransforms.Translate(sphereMesh, new Vector3d(0.5, 0.5, 0.5));
+
+            // Устанавливаем параметры для обрезки мешей
+            MeshMeshCut cutter = new MeshMeshCut();
+            cutter.Target = boxMesh;
+            cutter.CutMesh = sphereMesh;
+            cutter.Compute();
+            cutter.RemoveContained();
+
+
+            // Выполняем операцию обрезки
+            var success = cutter.Target;
+            {
+                // Результирующий меш после обрезки
+
+                // Сохраняем результирующий меш в файл или визуализируем
+                StandardMeshWriter.WriteMesh("cut_result.stl", success, WriteOptions.Defaults);
+                StandardMeshWriter.WriteMesh("boxGen.stl", boxMesh, WriteOptions.Defaults);
+                StandardMeshWriter.WriteMesh("sphereGen.stl", sphereMesh, WriteOptions.Defaults);
+
+                
+                Console.WriteLine("Обрезка успешно выполнена!");
+            }
+            {
+                Console.WriteLine("Обрезка не удалась!");
+            }
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("geometry 3 Sharp meshmesh.cut my example")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("geometry 3 Sharp meshmesh.cut my example")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("be525e67-9495-4c1d-9adc-2ba40b71bb94")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
